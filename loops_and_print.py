f8bb898c9fcdc880e17af4fe3d2321d293060104
GabrielaGuinez <162721892+GabrielaGuinez@users.noreply.github.com>
2024-05-23 23:47:06 -0300

    loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,18 +1,18 @@
 def enumerate_list(my_list):
-    colors = []
-    counter = 0
-    for index, elem in enumerate(my_list):
-        if elem != "":
-            colors.append(f"{counter}. {elem}")
-            counter += 1
+    colors=[]
+    counter=0
+    for index,elem in enumerate(my_list):
+        if elem!="":
+            colors.append(f"{counter}.{elem}")
+            counter+=1
     return colors
 
 def enumerate_backwards(my_list):
-    colors = []
-    counter = 0
-    for index, elem in enumerate(my_list):
-        if elem != "":
-            elem = elem[::-1]
+    colors=[]
+    counter=0
+    for index,elem in enumerate(my_list):
+        if elem!="":
+            elem=elem[::-1]
             colors.append(f"{counter}. {elem}")
-            counter += 1
+            counter+=1
     return colors
