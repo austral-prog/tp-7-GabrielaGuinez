c9d9834c30b678544a5118550ee97973f8b5f22d
GabrielaGuinez <162721892+GabrielaGuinez@users.noreply.github.com>
2024-05-23 23:39:05 -0300

    entregaloops.py

--- loops.py
+++ loops.py
@@ -1,18 +1,37 @@
-def index_of_by_index(word, list, index):
+def index_of_by_index(word, mylist, index):
+    for i in range(index,len(mylist)):
+        if mylist[i] == word:
+            return i 
     return -1
 
 
-def index_of_empty(list):
+def index_of_empty(mylist):
+    for i, item in enumerate(mylist)
+        if item == word:
+            return i 
     return -1
 
 
-def index_of(word, list):
+def index_of(word, mylist):
+    for i, item in enumerate(mylist):
+        if item == word:
+            return i 
     return -1
 
 
-def put(word, list):
+def put(word, mylist):
+    for i, item in enumerate(mylist):
+        if item == " ":
+            list[1] = word
+            return i 
     return -1
 
 
-def remove(word, list):
+def remove(word, mylist):
+    count = 0
+    for i, item in enumerate(mylist)
+        if item == word:
+            mylist[i] == " "
+            count += 1
+        return count
     return -1
