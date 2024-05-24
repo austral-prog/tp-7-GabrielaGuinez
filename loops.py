c406f1c45847ea71d51b7d5b4d8d36e04dfe25af
GabrielaGuinez <162721892+GabrielaGuinez@users.noreply.github.com>
2024-05-24 00:20:15 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,34 +1,18 @@
-def index_of_by_index(word, mylist, index):
-    for i in range(index,len(mylist)):
-        if mylist[i] == word:
-            return i 
-    return -1
+def enumerate_list(my_list):
+    colors = []
+    counter = 0
+    for index, elem in enumerate(my_list):
+        if elem != "":
+            colors.append(f"{counter}. {elem}")
+            counter += 1
+    return colors
 
-
-def index_of_empty(mylist):
-    for i,item in enumerate(mylist):
-        if item == " ":
-            return i 
-    return -1
-
-
-def index_of(word, mylist):
-    for i,item in enumerate(mylist):
-        if item == word:
-            return i 
-    return -1
-
-def put(word, mylist):
-    for i,item in enumerate(mylist):
-        if item == " ":
-            list[1] = word
-            return i 
-    return -1
-
-def remove(word, mylist):
-    count = 0
-    for i, item in enumerate(mylist):
-        if item == word:
-            mylist[i] == " "
-            count += 1
-    return count
+def enumerate_backwards(my_list):
+    colors = []
+    counter = 0
+    for index, elem in enumerate(my_list):
+        if elem != "":
+            elem = elem[::-1]
+            colors.append(f"{counter}. {elem}")
+            counter += 1
+    return colors
